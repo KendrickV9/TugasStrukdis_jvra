71990b1810f9a2c121ca0ec43efc55a8063d939d
KumaraIvanA <ivananuraga5@gmail.com>
2025-12-29 22:19:49 +0700

    Tambahkan method untuk mendapatkan alokasi warna graph

--- src/tugasstrukdis/graph.py
+++ src/tugasstrukdis/graph.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from collections import defaultdict
 import matplotlib.pyplot as plt
+import gcol
 import networkx as nx
 
 
@@ -9,13 +10,23 @@ class Graph:
         self.graph = nx.Graph()
         for v1, item in adj_matrix.items():
             for v2, adjacent in item.items():
-                if adjacent:
+                if v1 != v2 and adjacent:
                     self.graph.add_edge(v1, v2)
+        self.node_colors: dict[str, int] = gcol.node_coloring(self.graph, opt_alg=1)
 
     def get_figure(self) -> plt.Figure:
         fig, ax = plt.subplots()
-        nx.draw(self.graph, with_labels=True, ax=ax)
+        nx.draw(
+            self.graph,
+            with_labels=True,
+            ax=ax,
+            node_color=gcol.get_node_colors(self.graph, self.node_colors),
+        )
+
         return fig
 
     def get_colors(self) -> dict[str, int]:
-        return {}
+        return self.node_colors
+
+    def get_chromatic_number(self) -> int:
+        return max(self.node_colors.values()) + 1
